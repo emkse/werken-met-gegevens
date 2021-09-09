08851f4e7d8262b6a40d4ec90a403585ae811c36
emkse <99069595@mydavinci.nl>
2021-09-09 11:35:49 +0200

    Werkmetgegevensbestanden

---
+++ feestlunch.py
@@ -0,0 +1,7 @@
+croissant = int(17) # c will be 17
+prijs = int(2.78)
+korting = int(-1.5)
+prijs2 = int(0.39)
+stokbroden = int(2) # stokbroden will be 2
+
+print(croissant*prijs+prijs2*stokbroden+korting)
---
+++ gamehal.py
@@ -0,0 +1,7 @@
+Vrienden = int(input("Aantal vrienden: "))
+Toegangticket = float(7.45)
+Minuten = int(45)
+Gameseatkosten = float(0.37)
+Totaal = (Vrienden*Toegangticket+Minuten*Gameseatkosten*Vrienden)
+
+print(Totaal)
